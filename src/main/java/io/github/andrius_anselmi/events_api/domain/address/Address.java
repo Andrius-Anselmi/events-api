b7f4e203e5f45331bad20fae6db05ddc3b162e7f
andrius anselmi <andriusanselmi12@gmail.com>
2026-07-21 14:05:26 -0300

    feat: Creating entities

---
+++ src/main/java/io/github/andrius_anselmi/events_api/domain/address/Address.java
@@ -0,0 +1,31 @@
+package io.github.andrius_anselmi.events_api.domain.address;
+
+import io.github.andrius_anselmi.events_api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String city;
+
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/io/github/andrius_anselmi/events_api/domain/event/Event.java
@@ -0,0 +1,41 @@
+package io.github.andrius_anselmi.events_api.domain.event;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "event")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String title;
+
+    private String description;
+
+    private String imgUrl;
+
+    private String eventUrl;
+
+    private Boolean remote;
+
+    private Date date;
+
+
+
+
+
+
+}
